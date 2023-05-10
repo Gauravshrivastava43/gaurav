f38b3767a793ac632ade20ce22d0a339f40af6eb
Gaurav Shrivastava <128239833+Gauravshrivastava43@users.noreply.github.com>
2023-05-10 14:32:30 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Gaurav Shrivastava</h1>
+      
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src="logo192.png" className="App-logo" alt="logo" />
+
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://gauravshrivastava43.github.io/rds-college-project/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          College Mangement System
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
